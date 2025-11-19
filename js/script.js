3d690af9ebd4b1ab4ab7e5ab199aa6af0b6937b8
bellaviagabriele03 <bellaviagabriele03@gmail.com>
2025-11-19 12:33:19 +0100

    fix: aggiunti elementi nel dom con il metodo "create element"

--- js/script.js
+++ js/script.js
@@ -2,25 +2,46 @@
 // console.log("ciao");
 
 
+function createCard(array){
+    const docElem = document.createElement("div")
+    docElem.classList.add("row", "justify-content-between", "g-20", "wrap")
+    let contentString = "";
+    array.forEach(function(elem){
+        contentString += `
+        <div class = "col">
+            <div class="card">
+                <img class="pin" src="./img/pin.svg" alt="">
+                <img class="main-img" src="${elem.url}" alt="">
+                <span class="sub-title">${elem.date}</span>
+                <h3 class="card-title">${elem.title}</h3>
+            </div>
+        </div>
+        `;
+        
+        
+    })
+    docElem.innerHTML = contentString;
+    mainElem.appendChild(docElem);
+    return docElem;
+    
+}
+
+
+
+
+
+
+
+
+
 //elemti prelevati dal DOM
-const rowElem = document.querySelector("#content")
+const mainElem = document.querySelector("#content")
+const overlayElem = document.querySelector("#overlay")
 
 axios.get("https://lanciweb.github.io/demo/api/pictures/").then(function (resp) {
     const dataArray = resp.data;
-    console.log(dataArray);
-    let contentString = "";
-     dataArray.forEach(elem => {
-        contentString += `
-            <div class="col">
-                <div class="card">
-                    <img class="pin" src="./img/pin.svg" alt="">
-                    <img class="main-img" src="${elem.url}" alt="">
-                    <span class="sub-title">${elem.date}</span>
-                    <h3 class="card-title">${elem.title}</h3>
-                </div>
-            </div>
-        `
-    rowElem.innerHTML = contentString;
-    });
+    createCard(dataArray)
 
 })
+
+
